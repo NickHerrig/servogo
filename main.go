46aa80db5d602e8d9a98a84f738407aedb4e0021
nickherrig <neherrig@gmail.com>
2020-10-06 18:31:38 -0500

    Add env var for servo drive id, go fmt

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/jacobsa/go-serial/serial"
 )
@@ -10,6 +11,12 @@ import (
 func main() {
 
 	p := os.Getenv("SERVO_USB_PORT")
+	i := os.Getenv("SERVO_DRIVE_ID")
+
+	id, err := strconv.Atoi(i)
+	if err != nil {
+		log.Fatalf("Failed to convert servo id env var to stringe: %v", err)
+	}
 
 	options := serial.OpenOptions{
 		PortName:        p,
@@ -25,5 +32,12 @@ func main() {
 	}
 
 	defer port.Close()
+
+	pkt = []byte{}
+
+	n, err := port.Write(pk)
+	if err != nil {
+		log.Fatalf("port.Write: %v", err)
+	}
 
 }
