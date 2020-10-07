3aba97b8d6f9883a48e781b3a31a5d1f54adda0b
nickherrig <neherrig@gmail.com>
2020-10-06 22:14:48 -0500

    Add stop motor hard coded packet, serial connected

--- main.go
+++ main.go
@@ -2,54 +2,52 @@ package main
 
 import (
 	"log"
-	//	"os"
-	"fmt"
-	//	"strconv"
+	"os"
+	"strconv"
+	"time"
 
-	"go.bug.st/serial"
+	"github.com/tarm/serial"
 )
 
 func main() {
 
-	//	p := os.Getenv("SERVO_USB_PORT")
-	//	i := os.Getenv("SERVO_DRIVE_ID")
-	//
-	//	id, err := strconv.Atoi(i)
-	//	if err != nil {
-	//		log.Fatalf("Failed to convert servo id env var to stringe: %v", err)
-	//	}
+	p := os.Getenv("SERVO_USB_PORT")
+	i := os.Getenv("SERVO_DRIVE_ID")
+	id, err := strconv.Atoi(i)
+	if err != nil {
+		log.Fatalf("Failed to convert servo id env var to stringe: %v", err)
+	}
+	bid := byte(id)
 
-	ports, err := serial.GetPortsList()
+	c := &serial.Config{
+		Name:        p,
+		Baud:        38400,
+		Size:        8,
+		ReadTimeout: time.Millisecond * 500,
+	}
+
+	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	for _, port := range ports {
-		fmt.Printf("Found port: %v\n", port)
-	}
-
-	mode := &serial.Mode{
-		BaudRate: 38400,
-		Parity:   serial.NoParity,
-		DataBits: 8,
-		StopBits: serial.OneStopBit,
-	}
-
-	port, err := serial.Open("/dev/cu.usbserial-DN04SV8H", mode)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer port.Close()
+	defer s.Close()
 
 	//forward packet = x02 xe3 xbd xfe xc9 x80 xe9
 	//stop packet = x02 x83 x80 x85
 
-	stop := []byte{0x02, 0x83, 0x80, 0x85}
+	stop := []byte{bid, 0x83, 0x80, 0x85}
 
-	n, err := port.Write(stop)
+	n, err := s.Write(stop)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(n)
+	buf := make([]byte, 128)
+	n, err = s.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("%q", buf[:n])
+
 }
