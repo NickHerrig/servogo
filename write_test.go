4b55fb0d7c3e4141dd4a94bdff408f2a06037c2d
nickherrig <neherrig@gmail.com>
2020-10-08 22:52:07 -0500

    Code Cov 100% for packetLength

--- write_test.go
+++ write_test.go
@@ -1,30 +1,44 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestPacketLength(t *testing.T) {
 	tests := []struct {
 		name      string
 		input     int
 		want      byte
-		wantError error
+		wantError error 
 	}{
-		{
-			name:      "one positive data packet",
-			input:     50,
-			want:      0x80,
-			wantError: nil,
-		},
+		{"one positive data packet", 50, 0x80, nil},
+		{"one negative data packet", -50, 0x80, nil},
+		{"two positive data packets", 8000, 0xa0, nil},
+		{"two negative data packets", -8000, 0xa0, nil},
+		{"three positive data packets", 1000000, 0xc0, nil},
+		{"three negative data packets", -1000000, 0xc0, nil},
+		{"four positive data packets", 130000000, 0xe0, nil},
+		{"four negative data packets", -130000000, 0xe0, nil},
+		{"five positive data packets", 140000000, 0, errors.New("data out of range for dmm servo!")},
+		{"five negative data packets", -140000000, 0, errors.New("data out of range for dmm servo!")},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			pl, err := packetLength(tt.input)
-			if pl != tt.want {
-				t.Errorf("want %q; got %q", tt.want, pl)
-			}
-			if err != tt.wantError {
-				t.Errorf("want %q; got %q", tt.wantError, err)
-			}
+
+            if err != nil{
+			    if errors.Is(err, tt.wantError) {
+			      	t.Errorf("want %q; got %q", tt.wantError, err)
+                }
+            }else{
+			    if pl != tt.want {
+			    	t.Errorf("want %q; got %q", tt.want, pl)
+			    }
+			    if err != tt.wantError {
+			    	t.Errorf("want %q; got %q", tt.wantError, err)
+			    }
+            } 
 		})
 	}
 }
