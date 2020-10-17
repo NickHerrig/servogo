99ccf753529684b51c0b516efd3bb71ecb58e9a3
nickherrig <neherrig@gmail.com>
2020-10-17 18:08:46 -0500

    Add comments and TODO for data validation

--- input.go
+++ input.go
@@ -14,13 +14,18 @@ func ValidateInput(c string, d int) error{
         "stop": {0, 0},
     }
 
+    //Check that user passed a command
     if c == "" {
         return errors.New("Servo command is required!")
     }
 
+    //Check that user passed a valid command
 	if  _, ok := validInputs[c]; !ok {
 		return errors.New("Command not Implemeneted") 
 	}
+
+    //Check that user passed valid data for command
+    //TODO
     
     return nil
 }
