43d775d0d31468956a5e790585f204a2cdbfc253
nickherrig <neherrig@gmail.com>
2020-10-17 18:07:29 -0500

    Add command implemeneted check

--- input.go
+++ input.go
@@ -4,9 +4,23 @@ import (
     "errors"
 )
 
+type Values struct {
+    min, max int
+}
+
 func ValidateInput(c string, d int) error{
+
+    validInputs := map[string]Values {
+        "stop": {0, 0},
+    }
+
     if c == "" {
         return errors.New("Servo command is required!")
     }
+
+	if  _, ok := validInputs[c]; !ok {
+		return errors.New("Command not Implemeneted") 
+	}
+    
     return nil
 }
