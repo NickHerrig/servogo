30ac080e5729037142facdbd8b4ab3758db4bb44
nickherrig <neherrig@gmail.com>
2020-10-18 19:28:24 -0500

    Change command map to include codes, global var

--- validation.go
+++ validation.go
@@ -4,35 +4,38 @@ import (
 	"errors"
 )
 
-type Values struct {
-	min, max int
+type Functions struct {
+	min      int
+	max      int
+	function string
+	code     byte
 }
 
 var MissingCommandError = errors.New("No command sent, motor command is required")
 var InvalidCommandError = errors.New("Invalid command sent, send an valid command")
 var InvalidDataError = errors.New("Invalid data sent for command")
 
-func ValidateInput(c string, d int) error {
-	//Map of commands and valid data ranges for input validation
-	validInputs := map[string]Values{
-		"stop":      {0, 0},
-		"forwards":  {0, 0},
-		"backwards": {0, 0},
-	}
+//Map of commands and valid data ranges for input validation
+var commandMap = map[string]Functions{
+	"stop":      {0, 0, "Go_Relative_Pos", 0x03},
+	"forwards":  {0, 0, "Go_Relative_Pos", 0x03},
+	"backwards": {0, 0, "Go_Relative_Pos", 0x03},
+}
 
+func ValidateInput(c string, d int) error {
 	//Check that user passed a command
 	if c == "" {
 		return MissingCommandError
 	}
 
 	//Check that user passed a valid command
-	if _, ok := validInputs[c]; !ok {
+	if _, ok := commandMap[c]; !ok {
 		return InvalidCommandError
 	}
 
 	//Check that user passed valid data for command
-	min := validInputs[c].min
-	max := validInputs[c].max
+	min := commandMap[c].min
+	max := commandMap[c].max
 	if d < min || d > max {
 		return InvalidDataError
 	}
