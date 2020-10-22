a9815849bdf19e909481c8b6776ffef6181a47a2
nickherrig <neherrig@gmail.com>
2020-10-21 21:25:41 -0500

    Add comments, and reorg command map

--- functions.go
+++ functions.go
@@ -1,16 +1,18 @@
 package main
 
+// Functions struct groups dmm funcs and implementation details
 type Functions struct {
-	min      int
-	max      int
-	function string
-	code     byte
-	data     int
+	function string // the dmm motor function
+	min      int    // the minimum data value for a command
+	max      int    // the maximum data value for a command
+	code     byte   // the dmm motor function code in hexadecimal
+	data     int    // the data for a particular command if 0
 }
 
-//Map of commands and valid data ranges for input validation
+//Map of commands dmm function implementation detail struct
 var commandMap = map[string]Functions{
-	"stop":      {0, 0, "Go_Relative_Pos", 0x03, 0},
-	"forwards":  {0, 0, "Go_Relative_Pos", 0x03, 13000000},
-	"backwards": {0, 0, "Go_Relative_Pos", 0x03, -13000000},
+	"stop":      {"Go_Relative_Pos", 0, 0, 0x03, 0},
+	"forwards":  {"Go_Relative_Pos", 0, 0, 0x03, 13000000},
+	"backwards": {"Go_Relative_Pos", 0, 0, 0x03, -13000000},
+	"send-to":   {"Go_Absolute_Pos", 0, 0, 0x01, 0},
 }
