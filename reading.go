fe0e270559077cc4b874c33f44ae6fb134155bec
nickherrig <neherrig@gmail.com>
2020-10-23 14:31:34 -0500

    Add reading.go and tests, some notes for next time

--- reading.go
+++ reading.go
@@ -6,11 +6,29 @@ import (
 
 var InvalidChecksumError = errors.New("validateChecksum(): The checksum packet was not valid.")
 
-func validateChecksum(r []byte) (bool, error) {
-	return true, nil
+func validateChecksum(p []byte) error {
+	csb := p[len(p)-1] // checksum byte is last byte in packet
+    bp := p[:len(p)-1]  // begining of the packet
+    cs := checksumByte(bp)
+    if csb == cs {
+        return nil
+    } else{
+        return InvalidChecksumError
+    }
 }
 
-func ParsePacket(r []byte) (string, error) {
-	//validateChecksum(r)
-	return "motor response", nil
+func ParsePacket(p []byte) (string, error) {
+	// Validate checksum byte from response
+    err := validateChecksum(p)
+	if err != nil {
+        return "", err
+    }
+    /*TODO:
+      Think through and design parsing of data...
+      Packet length can be calculated with len(packet), is this needed? actually?
+      need function to grab motor function code?
+      parse data differently deppending on the function code?
+    */
+
+	return "parsed pkt", nil
 }
---
+++ reading_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "testing"
+    "errors"
+)
+
+func TestValidateChecksum(t *testing.T) {
+	tests := []struct {
+		name    string
+		packet  []byte 
+		want    error
+	}{
+		{"valid checksum",[]byte{0x02, 0x83, 0x01, 0x86}, nil},
+		{"invalid checksum",[]byte{0x02, 0x83, 0x01, 0x88}, InvalidChecksumError},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := validateChecksum(tt.packet)
+			if err != nil {
+				if errors.Is(err, tt.want) != true {
+					t.Errorf("want %q; got %q", tt.want, err)
+				}
+			}
+		})
+	}
+}
