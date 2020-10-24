6cfa70900516b8c1a45f7c0f3c56853baae41baf
nickherrig <neherrig@gmail.com>
2020-10-24 15:56:20 -0500

    grab func code, TODO parse accordoingly, go fmt

--- reading.go
+++ reading.go
@@ -8,27 +8,32 @@ var InvalidChecksumError = errors.New("validateChecksum(): The checksum packet w
 
 func validateChecksum(p []byte) error {
 	csb := p[len(p)-1] // checksum byte is last byte in packet
-    bp := p[:len(p)-1]  // begining of the packet
-    cs := checksumByte(bp)
-    if csb == cs {
-        return nil
-    } else{
-        return InvalidChecksumError
-    }
+	bp := p[:len(p)-1] // begining of the packet
+	cs := checksumByte(bp)
+	if csb == cs {
+		return nil
+	} else {
+		return InvalidChecksumError
+	}
 }
 
 func ParsePacket(p []byte) (string, error) {
 	// Validate checksum byte from response
-    err := validateChecksum(p)
+	err := validateChecksum(p)
 	if err != nil {
-        return "", err
-    }
-    /*TODO:
-      Think through and design parsing of data...
-      Packet length can be calculated with len(packet), is this needed? actually?
-      need function to grab motor function code?
-      parse data differently deppending on the function code?
-    */
+		return "", err
+	}
+	// grab function code from byte two
+	fc := p[1] | 0x1F
+
+	// deppending on func code prase data differently
+	if fc == 0x19 {
+		//TODO: parse IS_STATUS return byte
+	} else if fc == 0x1A {
+		//TODO: parse IS_CONFIG return byte
+	} else {
+		//TODO: Parse normal data!
+	}
 
 	return "parsed pkt", nil
 }
--- reading_test.go
+++ reading_test.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "testing"
-    "errors"
+	"errors"
+	"testing"
 )
 
 func TestValidateChecksum(t *testing.T) {
 	tests := []struct {
-		name    string
-		packet  []byte 
-		want    error
+		name   string
+		packet []byte
+		want   error
 	}{
-		{"valid checksum",[]byte{0x02, 0x83, 0x01, 0x86}, nil},
-		{"invalid checksum",[]byte{0x02, 0x83, 0x01, 0x88}, InvalidChecksumError},
+		{"valid checksum", []byte{0x02, 0x83, 0x01, 0x86}, nil},
+		{"invalid checksum", []byte{0x02, 0x83, 0x01, 0x88}, InvalidChecksumError},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
