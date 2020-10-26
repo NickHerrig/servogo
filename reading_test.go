8ea08861213c8c640ebc24a9377984573c30975a
nickherrig <neherrig@gmail.com>
2020-10-25 19:23:06 -0500

    Add tests for parseData() func

--- reading_test.go
+++ reading_test.go
@@ -5,6 +5,31 @@ import (
 	"testing"
 )
 
+func TestParseData(t *testing.T) {
+	tests := []struct {
+		name  string
+		data  []byte
+		want  int 
+	}{
+		{"one positive data packet", []byte{0xBC}, 60},
+		{"one negative data packet", []byte{0xC4}, -60},
+		{"two positive data packet", []byte{0x81, 0x96}, 150},
+		{"two negative data packet", []byte{0xFE, 0xEA}, -150},
+		{"three positive data packet", []byte{0x80, 0xC6, 0xA8}, 9000},
+		{"three negative data packet", []byte{0xFF, 0xB9, 0xD8}, -9000},
+		{"four positive data packet", []byte{0xB9, 0x9C, 0x9C, 0x80}, 120000000},
+		{"four negative data packet", []byte{0xC6, 0xE3, 0xE4, 0x80}, -120000000},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			i := parseData(tt.data)
+			if i != tt.want {
+				t.Errorf("want %d; got %d", tt.want, i)
+			}
+		})
+	}
+}
+
 func TestValidateChecksum(t *testing.T) {
 	tests := []struct {
 		name   string
