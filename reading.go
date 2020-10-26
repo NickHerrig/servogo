9bd8e47a96467aa60c1a3934806dccee0ec3e68f
nickherrig <neherrig@gmail.com>
2020-10-25 19:22:38 -0500

    Add parseData() func to parse signed data from motor

--- reading.go
+++ reading.go
@@ -6,9 +6,19 @@ import (
 
 var InvalidChecksumError = errors.New("validateChecksum(): The checksum packet was not valid.")
 
+func parseData(d []byte) int{
+	p := int(d[0] & 0x7f)
+	p = (p & (0x40 - 1)) - (p & 0x40)
+	for _, b := range d[1:]{
+		p = (p << 7) + int(b & 0x7f)
+	}
+
+    return p
+}
+
 func validateChecksum(p []byte) error {
 	csb := p[len(p)-1] // checksum byte is last byte in packet
-	bp := p[:len(p)-1] // begining of the packet
+	bp := p[:len(p)-1] // slice of the rest of the packet
 	cs := checksumByte(bp)
 	if csb == cs {
 		return nil
@@ -17,23 +27,30 @@ func validateChecksum(p []byte) error {
 	}
 }
 
-func ParsePacket(p []byte) (string, error) {
+func ParsePacket(p []byte) (int, error) {
 	// Validate checksum byte from response
 	err := validateChecksum(p)
 	if err != nil {
-		return "", err
+		return 0, err
 	}
 	// grab function code from byte two
 	fc := p[1] | 0x1F
-
+	//TODO grab data bytes 
+	d := []byte{0x01, 0x02} 
+    
+    var msg int 
 	// deppending on func code prase data differently
 	if fc == 0x19 {
 		//TODO: parse IS_STATUS return byte
 	} else if fc == 0x1A {
 		//TODO: parse IS_CONFIG return byte
 	} else {
-		//TODO: Parse normal data!
+        msg = parseData(d)
 	}
+    
+    if err != nil {
+        return 0, err
+    }
 
-	return "parsed pkt", nil
+	return msg, nil
 }
